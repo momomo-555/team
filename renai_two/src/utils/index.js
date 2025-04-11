8646b098b29bfa4ad25951325fe0398a77000b75
momomo-555 <3325148001@qq.com>
2025-04-11 14:27:10 +0800

    Delete renai_two/src directory

--- renai_two/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-//中转
-import { setToken, getToken, removeToken } from './token'
-import { request } from './requset'
-import { requestToken } from './requestToken'
-import { mapStatus } from './status'
-export { setToken, getToken, removeToken, request, requestToken, mapStatus }
